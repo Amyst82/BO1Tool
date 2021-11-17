a959fdfdad30e3e7a5fe91ad8b6422b7ad56b999
Amyst82 <sergi.simple@yandex.ru>
2021-11-17 19:49:53 +0300

    Add files via upload

---
+++ Converters/ColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace bo1tool.Converters
+{
+    class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string res = (string)((double)value / 255).ToString("N2");
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val = double.Parse((string)value);
+            val = val * 255;
+            return val;
+        }
+    }
+}
